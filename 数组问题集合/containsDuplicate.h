4cc9d329525a62b7a1cfa4f958641ca406e9f05f
hanzhongping <360420509@qq.com>
2020-04-22 20:53:18 +0800

    存在重复元素

---
+++ 数组问题集合/containsDuplicate.h
@@ -0,0 +1,80 @@
+/************************************************************************/
+/** 
+* 题目：存在重复元素
+* 给定一个整数数组，判断是否存在重复元素。
+
+* 如果任意一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+* 
+* 示例 1:
+* 输入: [1,2,3,1]
+* 输出: true
+* 
+* 示例 2:
+* 输入: [1,2,3,4]
+* 输出: false
+* 
+* 示例 3:
+* 输入: [1,1,1,3,3,4,3,2,4,2]
+* 输出: true
+*/
+/************************************************************************/
+
+#pragma once
+#include <vector>
+#include <map>
+#include <set>
+#include <iostream>
+
+using namespace std;
+namespace Solution
+{
+	bool containsDuplicate(vector<int>& nums) 
+	{
+		int len = nums.size();
+		if (len <= 1)
+		{
+			return false;
+		}
+
+		map<int, int> tmp;
+		for (auto val : nums)
+		{
+			auto iter = tmp.find(val);
+			if (iter != tmp.end())
+			{
+				return true;
+			}
+			else
+			{
+				tmp[val] = 1;
+			}
+		}
+		
+		return false;
+	}
+
+	bool containsDuplicate2(vector<int>& nums) 
+	{
+		int len = nums.size();
+		if (len <= 1)
+		{
+			return false;
+		}
+
+		set<int> tmp;
+		for (int i = 0; i < len; ++i)
+		{
+			if (tmp.find(nums[i]) != tmp.end())
+			{
+				return true;
+			} 
+			else
+			{
+				tmp.insert(nums[i]);
+			}
+
+		}
+
+		return false;
+	}
+}
--- 数组问题集合/main.cpp
+++ 数组问题集合/main.cpp
@@ -1,40 +1,33 @@
 #pragma once
 #include <vector>
+#include <map>
 #include <iostream>
 
 #include "rotateArray.h"
-
+#include "containsDuplicate.h"
 
 int main()
 {
+	
 	// 测试用例1：未初始化的数组
 	vector<int> tst1;
 
 	// 示例2
-	int arr[] = {1,2,3,4,5,6,7};
+	int arr[] = {1,2,3,1};
 	vector<int> tst2(arr, arr + sizeof(arr) / sizeof(int));
 
 	// 示例3
-	int arr3[] = {-1,-100,3,99};
+	int arr3[] = {1,2,3,4};
 	vector<int> tst3(arr3, arr3 + sizeof(arr3) / sizeof(int));
 
 	// 示例4
-	int arr4[] = {1,2};
+	int arr4[] = {1,1,1,3,3,4,3,2,4,2};
 	vector<int> tst4(arr4, arr4 + sizeof(arr4) / sizeof(int));
 	
-	Solution::rotate(tst4, 3);
-	for (auto val : tst4)
-	{
-		cout<<val<<" ";
-	}
-	cout<<endl;
-
-	Solution::rotate(tst3, 6);
-	for (auto val : tst3)
-	{
-		cout<<val<<" ";
-	}
-	cout<<endl;
+	bool flag1 = Solution::containsDuplicate2(tst1);
+	bool flag2 = Solution::containsDuplicate2(tst2);
+	bool flag3 = Solution::containsDuplicate2(tst3);
+	bool flag4 = Solution::containsDuplicate2(tst4);
 
 	return 0;
 }
