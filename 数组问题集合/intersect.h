bfeb5136251dc88252a4fa42c2ab6a6798b79a01
hanzhongping <360420509@qq.com>
2020-05-11 20:35:46 +0800

    求交集

--- 数组问题集合/intersect.h
+++ 数组问题集合/intersect.h
@@ -29,6 +29,72 @@ namespace Solution
 {
 	vector<int> intersect(vector<int>& nums1, vector<int>& nums2) 
 	{
+		vector<int> ret;
+		int cnt = 0;	// 计数器
+		if (!nums1.empty() && !nums2.empty())
+		{
+			auto iter1 = nums1.begin();
+			while (iter1 != nums1.end())
+			{
+				auto iter2 = nums2.begin();
+				while (iter2 != nums2.end())
+				{
+					if (*iter1 == *iter2)
+					{
+						ret.push_back(*iter1);
+						iter2 = nums2.erase(iter2);
+						break;
+					}
+					else
+					{
+						++iter2;
+					}
+					++cnt;
+				}
+				++iter1;
+				++cnt;
+			}
+		}
 
+		cout<<"intersect count = "<<cnt<<endl;
+		return ret;
+	}
+
+	// nums1和nums2都有序
+	vector<int> intersect_sorted(vector<int>& nums1, vector<int>& nums2) 
+	{
+		vector<int> ret;
+		int cnt = 0;
+		if (nums1.empty() || nums2.empty())
+		{
+			return ret;
+		}
+
+		if (nums1[0] > nums2[nums2.size() - 1] || nums2[0] > nums1[nums1.size() - 1])
+		{
+			return ret;
+		}
+		int i = 0, j = 0; 
+		while (i < nums1.size() && j < nums2.size())
+		{
+			if (nums1[i] < nums2[j])
+			{
+				++i;
+			} 
+			else if (nums1[i] > nums2[j])
+			{
+				++j;
+			} 
+			else
+			{
+				ret.push_back(nums1[i]);
+				++i;
+				++j;
+			}
+			++cnt;
+		}
+
+		cout<<"intersect_sorted count = "<<cnt<<endl;
+		return ret;
 	}
 }
--- 数组问题集合/main.cpp
+++ 数组问题集合/main.cpp
@@ -3,29 +3,46 @@
 #include "rotateArray.h"
 #include "containsDuplicate.h"
 #include "singleNumber.h"
+#include "intersect.h"
 
 int main()
 {
+	__int64 xxx = 0;
+	cout<<sizeof(long long)<<endl;
 	
 	// 测试用例1：未初始化的数组
 	vector<int> tst1;
 
 	// 示例2
-	int arr[] = {2,2,1};
+	int arr[] = {1,2,2,1};
 	vector<int> tst2(arr, arr + sizeof(arr) / sizeof(int));
 
 	// 示例3
-	int arr3[] = {4,1,2,1,2};
+	int arr3[] = {2,2};
 	vector<int> tst3(arr3, arr3 + sizeof(arr3) / sizeof(int));
 
 	// 示例4
-	int arr4[] = {1,1,3,3,4,2,4,2,5,7,7};
+	int arr4[] = {4,9,5};
 	vector<int> tst4(arr4, arr4 + sizeof(arr4) / sizeof(int));
 	
-	int num1 = Solution::singleNumber3(tst1);
-	int num2 = Solution::singleNumber3(tst2);
-	int num3 = Solution::singleNumber3(tst3);
-	int num4 = Solution::singleNumber3(tst4);
+	// 示例5
+	int arr5[] = {9,4,9,8,4};
+	vector<int> tst5(arr5, arr5 + sizeof(arr5) / sizeof(int));
+
+	vector<int> ret1 = Solution::intersect(tst1, tst1);
+	vector<int> ret2 = Solution::intersect(tst1, tst2);
+	vector<int> ret3 = Solution::intersect(tst2, tst3);
+	vector<int> ret4 = Solution::intersect(tst4, tst5);
+
+	// sorted示例1
+	int arr6[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
+	vector<int> tst6(arr6, arr6 + sizeof(arr6) / sizeof(int));
+
+	// sorted示例2
+	int arr7[] = {1,2,3,4};
+	vector<int> tst7(arr7, arr7 + sizeof(arr7) / sizeof(int));
+
+	vector<int> ret5 = Solution::intersect_sorted(tst6, tst7);
 
 	return 0;
 }
