6d1a535e985f5b20dc37aac0ffc96e808bc9afad
hanzhongping <360420509@qq.com>
2020-06-01 20:58:32 +0800

    旋转矩阵

--- 数组问题集合/main.cpp
+++ 数组问题集合/main.cpp
@@ -8,83 +8,54 @@
 #include "moveZeroes.h"
 #include "twoSum.h"
 #include "isValidSudoku.h"
+#include "rotate.h"
 
-int main()
+/************************************************************************/
+/* 全局测试数据                                                                     */
+/************************************************************************/
+// 测试用例1：未初始化的数组
+vector<int> tst1;
+
+// 示例2
+int arr[] = {2, 7, 11, 15};
+vector<int> tst2(arr, arr + sizeof(arr) / sizeof(int));
+
+// 示例3
+int arr3[] = {3,3};
+vector<int> tst3(arr3, arr3 + sizeof(arr3) / sizeof(int));
+
+// 示例4
+int arr4[] = {9,0,9};
+vector<int> tst4(arr4, arr4 + sizeof(arr4) / sizeof(int));
+
+// 示例5
+int arr5[] = {9,0,9,0,0};
+vector<int> tst5(arr5, arr5 + sizeof(arr5) / sizeof(int));
+
+// sorted示例1
+int arr6[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
+vector<int> tst6(arr6, arr6 + sizeof(arr6) / sizeof(int));
+
+// sorted示例2
+int arr7[] = {1,2,3,4};
+vector<int> tst7(arr7, arr7 + sizeof(arr7) / sizeof(int));
+
+void testMoveZeros()
 {
-	/*map<int, int> m_unit;
-	for (int i = 0; i < 9; ++i)
-	{
-		for (int j = 0; j < 9; ++j)
-		{
-			int key = i / 3 * 10 + j / 3;
-			if (m_unit.find(key) != m_unit.end())
-			{
-				m_unit[key] += 1;
-			} 
-			else
-			{
-				m_unit[key] = 1;
-			}
-		}
-	}
-
-	cout<<m_unit.size()<<endl;*/
-
-	/*char a[] = 'aaa',b[]='aaa';
-	string A = 'AAA', B = 'AAA';
-	cout << '*a和*b的值分别是：' << &a << '，' << &b << endl;
-	cout << '*a和*b的值分别是：' << &(*a) << '，' << &(*b) << endl;
-	cout << '*“aaa”的值是：'<< &'aaa' << endl;
-	cout << '利用 == 比较a,b两个字符串，结果是（相等为1，不等为0）：'			<< (a == b ? 1 : 0)				<< endl;
-	cout << '利用strcmp()比较a，b两个字符串，结果是（相等为0，不等非0）：'	<< (strcmp(a, b) == 0 ? 1 : 0)	<< endl;
-	cout << '利用 == 比较A,B两个string，结果是（相等为1，不等为0）：'		<< (A == B ? 1 : 0)				<< endl;
-	cout << '利用compare()比较A,B两个string，结果是（相等为0，不等非0）：'	<< (A.compare(B) == 0 ? 1 : 0)	<< endl;*/
-	//int x = 10;
-	//void* p = &x;
-	//cout << sizeof(void*) << ' address' << p << endl;
-
-	int autoarr[] = {1,2,3,4};
-	for (auto id : autoarr)
-	{
-		cout<<id<<endl;
-	}
-	__int64 xxx = 0;
-	cout<<sizeof(long long)<<endl;
-	
-	// 测试用例1：未初始化的数组
-	vector<int> tst1;
-
-	// 示例2
-	int arr[] = {2, 7, 11, 15};
-	vector<int> tst2(arr, arr + sizeof(arr) / sizeof(int));
-
-	// 示例3
-	int arr3[] = {3,3};
-	vector<int> tst3(arr3, arr3 + sizeof(arr3) / sizeof(int));
-
-	// 示例4
-	int arr4[] = {9,0,9};
-	vector<int> tst4(arr4, arr4 + sizeof(arr4) / sizeof(int));
-	
-	// 示例5
-	int arr5[] = {9,0,9,0,0};
-	vector<int> tst5(arr5, arr5 + sizeof(arr5) / sizeof(int));
-
-	// sorted示例1
-	int arr6[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
-	vector<int> tst6(arr6, arr6 + sizeof(arr6) / sizeof(int));
-
-	// sorted示例2
-	int arr7[] = {1,2,3,4};
-	vector<int> tst7(arr7, arr7 + sizeof(arr7) / sizeof(int));
-
-	vector<int> ret1, ret2, ret3;
-
 	Solution::moveZeroes(tst1);
+	Solution::moveZeroes(tst5);
+}
+
+void testTwoSum()
+{
+	vector<int> ret1, ret2, ret3;
 	ret1 = Solution::twoSum2(tst1, 9);
 	ret2 = Solution::twoSum2(tst2, 9);
 	ret3 = Solution::twoSum2(tst3, 6);
+}
 
+void testSudo()
+{
 	char sudo1[9][9] = { 
 		{'5','3','.','.','7','.','.','.','.'},
 		{'6','.','.','1','9','5','.','.','.'},
@@ -119,8 +90,40 @@ int main()
 			sudo2_v[i].push_back(sudo2[i][j]);
 		}
 	}
-	
+
 	bool isSudo = Solution::isValidSudoku(sudo1_v);
 	isSudo = Solution::isValidSudoku(sudo2_v);
+}
+
+void testRotateMatrix()
+{
+	vector<vector<int>> matrix3_v(3);
+	vector<vector<int>> matrix4_v(4);
+	int matrix3[3][3] = { {1,2,3}, {4,5,6}, {7,8,9}};
+	int matrix4[4][4] = { {5, 1, 9,11}, {2, 4, 8,10}, {13, 3, 6, 7}, {15,14,12,16} };
+	
+	for (int i = 0; i < 3; ++i)
+	{
+		for (int j = 0; j < 3; ++j)
+		{
+			matrix3_v[i].push_back(matrix3[i][j]);
+		}
+	}
+	Solution::rotateMatrix(matrix3_v);
+
+	for (int i = 0; i < 4; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			matrix4_v[i].push_back(matrix4[i][j]);
+		}
+	}
+	Solution::rotateMatrix(matrix4_v);
+}
+
+int main()
+{
+	
+	testRotateMatrix();
 	return 0;
 }
